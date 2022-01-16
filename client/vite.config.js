d5dd67efee1bb770a1af5cc6e2f829d4176b192d
Darkryhr <gabyk800@gmail.com>
2022-01-16 13:58:32 +0200

    config stuff

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://sightsee.herokuapp.com',
         changeOrigin: true,
         secure: false,
       },
