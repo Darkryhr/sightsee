ab2c0788035fd85993c49ce617e58ef577afa0bf
Darkryhr <gabyk800@gmail.com>
2022-01-16 13:34:08 +0200

    folder structure change for heroku

--- test.session.sql
+++
@@ -1,2 +0,0 @@
--- @BLOCK
-DROP TABLE users;
--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,12 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(
-  'test',
+  process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
     dialect: 'mysql',
-    host: 'localhost',
+    host: process.env.HOST,
   }
 );
 
